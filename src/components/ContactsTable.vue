3ed1dcae7ed5d657f1293544cd1057460de5a94a
bkRahul <rahulsatya1068@gmail.com>
2019-05-23 12:37:28 +0530

    Added ContactsTable component

---
+++ src/components/ContactsTable.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="contacts-table">
+        <table>
+            <thead>
+                <tr>
+                    <th>Contact Name</th>
+                    <th>Contact Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>Richard Hendricks</td>
+                <td>richard@piedpiper.com</td>
+                </tr>
+                <tr>
+                <td>Bertram Gilfoyle</td>
+                <td>gilfoyle@piedpiper.com</td>
+                </tr>
+                <tr>
+                <td>Dinesh Chugtai</td>
+                <td>dinesh@piedpiper.com</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'contacts-table',
+}
+</script>
