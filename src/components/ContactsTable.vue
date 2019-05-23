f0fc5865afc97459288430b0323cbc2071464494
bkRahul <rahulsatya1068@gmail.com>
2019-05-23 23:18:11 +0530

    view to display saved contacts

--- src/components/ContactsTable.vue
+++ src/components/ContactsTable.vue
@@ -1,32 +1,29 @@
 <template>
-    <div id="contacts-table">
-        <table>
-            <thead>
-                <tr>
-                    <th>Contact Name</th>
-                    <th>Contact Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td>Richard Hendricks</td>
-                <td>richard@piedpiper.com</td>
-                </tr>
-                <tr>
-                <td>Bertram Gilfoyle</td>
-                <td>gilfoyle@piedpiper.com</td>
-                </tr>
-                <tr>
-                <td>Dinesh Chugtai</td>
-                <td>dinesh@piedpiper.com</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+  <div id="contacts-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Contact Name</th>
+          <th>Contact Email</th>
+          <th>Contact Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="contact in contacts" :key="contact.id">
+          <td>{{ contact.name }}</td>
+          <td>{{ contact.email }}</td>
+          <td>{{ contact.number }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'contacts-table',
-}
+  name: "contacts-table",
+  props: {
+    contacts: Array
+  }
+};
 </script>
