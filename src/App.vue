b37c898c0422d6a1a167471c0a181735a7a96035
bkRahul <rahulsatya1068@gmail.com>
2019-05-24 20:20:31 +0530

    added delete and update feature

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
-  <div id="app">
+  <div id="app" class="small-container">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to Your Vue.js Phone Book App</h1>
+
+    <contacts-table
+      v-bind:contacts="contacts"
+      @delete:contact="deleteContact"
+      @edit:contact="updateContact"
+    />
+
+    <contact-form @add:contact="addContact"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContactsTable from "./components/ContactsTable.vue";
+
+import ContactForm from "./components/ContactForm.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    ContactsTable,
+    ContactForm
+  },
+  data() {
+    return {
+      contacts: [
+        {
+          id: 1,
+          name: "Richard Hendricks",
+          email: "richard@piedpiper.com",
+          number: "8875695251"
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com",
+          number: "8875695251"
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com"
+        }
+      ]
+    };
+  },
+  methods: {
+    addContact(contact) {
+      const lastId =
+        this.contacts.length > 0
+          ? this.contacts[this.contacts.length - 1].id
+          : 0;
+      const id = lastId + 1;
+      const newContact = { ...contact, id };
+
+      this.contacts = [...this.contacts, newContact];
+    },
+    deleteContact(id) {
+      this.contacts = this.contacts.filter(contact => contact.id !== id);
+    },
+    updateContact(id, updatedContact) {
+        this.contacts = this.contacts.map(contact =>
+    contact.id === id ? updatedContact : contact);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
 </style>
--- src/components/ContactsTable.vue
+++ src/components/ContactsTable.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="contacts-table">
-    <table>
+    <p v-if="contacts.length < 1" class="empty-table">No contacts</p>
+    <table v-else>
       <thead>
         <tr>
           <th>Contact Name</th>
           <th>Contact Email</th>
           <th>Contact Number</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="contact in contacts" :key="contact.id">
-          <td>{{ contact.name }}</td>
-          <td>{{ contact.email }}</td>
-          <td>{{ contact.number }}</td>
+          <td v-if="editing === contact.id">
+            <input type="text" v-model="contact.name">
+          </td>
+          <td v-else>{{ contact.name }}</td>
+          <td v-if="editing === contact.id">
+            <input type="email" v-model="contact.email">
+          </td>
+          <td v-else>{{ contact.email }}</td>
+          <td v-if="editing === contact.id">
+            <input type="tel" size="20" minlength="10" maxlength="10" v-model="contact.number">
+          </td>
+          <td v-else>{{ contact.number }}</td>
+          <td v-if="editing === contact.id">
+            <button @click="editContact(contact)">Save</button>
+            <button class="muted-button" @click="editing = null">Cancel</button>
+          </td>
+          <td v-else>
+            <button @click="editMode(contact.id)">Edit</button>
+            <button @click="$emit('delete:contact', contact.id);">Delete</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -24,6 +43,27 @@ export default {
   name: "contacts-table",
   props: {
     contacts: Array
+  },
+  data() {
+    return {
+      editing: null
+    };
+  },
+  methods: {
+    editMode(id) {
+      this.editing = id;
+    },
+    editContact(contact) {
+      if (this.name === "" || this.email === "" || this.number === "") return;
+      this.$emit("edit:contact", contact.id, contact);
+      this.editing = null;
+    }
   }
 };
 </script>
+
+<style scoped>
+button {
+  margin: 0 0.5rem 0 0;
+}
+</style>
